ed90e11423e6188e51151afc86a9cf09cdd2f0db
elisheva <elishevatruz1@gmail.com>
2024-06-06 15:43:07 +0300

    gnn

--- GNN.py
+++ GNN.py
@@ -13,6 +13,7 @@ from itertools import combinations, product
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
+from torch.utils.tensorboard import SummaryWriter
 from newDataSet import * 
    
 
@@ -27,31 +28,31 @@ class LightGlue(nn.Module):
     def forward(self, xA, xB):
         
         S = torch.matmul(self.linear1(xA), self.linear1(xB).T)
-        print(S[:5,:5])
+        # print(S[:5,:5])
         sigma_A = torch.sigmoid(self.linear2(xA))
         sigma_B = torch.sigmoid(self.linear2(xB))
         
         softmax_SkA = F.softmax(S/100000, dim=1)
         softmax_SkB = F.softmax(S/100000, dim=0)
         P = torch.zeros_like(S)
-        print(sigma_A[:10])
-        print(sigma_B[:10]) 
-        print(softmax_SkA)
-        print(softmax_SkB)
+        # print(sigma_A[:10])
+        # print(sigma_B[:10]) 
+        # print(softmax_SkA)
+        # print(softmax_SkB)
 
         P = sigma_A @ sigma_B.T * softmax_SkB * softmax_SkA
 
 
         return sigma_A, sigma_B, P
     
-def loss(P, M, not_A, not_B, sigma_A, sigma_B):
+def loss_(P, M, not_A, not_B, sigma_A, sigma_B):
     epsilon = 1e-8
     loss=0
 
     M = torch.tensor(M, dtype=torch.long)
     not_A = torch.tensor(not_A, dtype=torch.long)
     not_B = torch.tensor(not_B, dtype=torch.long)
-    print("p:",P)
+    # print("p:",P)
     # print("sigma_a",sigma_A)
     correspondence_loss_sum = ((torch.log(P[M[:, 0], M[:, 1]]+epsilon) ).sum())/len(M)
     unmatchable_loss_A = 0.5 * ((torch.log(1 - sigma_A[not_A]+epsilon)).sum()) / len(not_A)
@@ -59,64 +60,60 @@ def loss(P, M, not_A, not_B, sigma_A, sigma_B):
     loss = correspondence_loss_sum + unmatchable_loss_A + unmatchable_loss_B 
 
     return -loss
-# def loss(P, M, not_A, not_B, sigma_A, sigma_B):
-#     loss = 0
-    
-#     # Correspondences loss term
-#     M_indices = torch.tensor(M, dtype=torch.long)
-#     correspondence_loss = torch.log(P)
+def train(model, optimizer, loader):
+    total_loss = 0
+    for data in loader.dataset:
+        optimizer.zero_grad()  # Clear gradients.
+        xA=torch.tensor(data['des1'],dtype=torch.float32)
+        xB=torch.tensor(data['des2'],dtype=torch.float32)
+        sigma_A, sigma_B, P = model(xA, xB)
 
-#     correspondence_loss_sum = correspondence_loss[M_indices[:, 0], M_indices[:, 1]].sum() / len(M_indices)
-    
-#     unmatchable_loss_A = 0.5 * torch.log(1 - sigma_A[not_A]).sum() / len(not_A)
-#     unmatchable_loss_B = 0.5 * torch.log(1 - sigma_B[not_B]).sum() / len(not_B)
-    
-#     loss = correspondence_loss_sum + unmatchable_loss_A + unmatchable_loss_B 
+        loss = loss_(P, data['m'], data['notA'], data['notB'], sigma_A, sigma_B)
 
-#     return -loss
-
-# def loss(P, M, not_A, not_B, sigma_A, sigma_B):
-#     loss = 0
-
-#     # Correspondences loss term (assuming M is a LongTensor of indices)
-#     M_indices = torch.tensor(M, dtype=torch.long)  # Convert M to LongTensor
-#     correspondence_loss = -F.log_softmax(P,dim=1)  # Negative log-softmax for cross-entropy
-#     correspondence_loss_sum = correspondence_loss[M_indices[:, 0], M_indices[:, 1]].sum() / len(M_indices)
-#     # Unmatchable loss terms
-#     unmatchable_loss_A = -torch.mean(F.log_softmax(torch.cat((sigma_A, 1 - sigma_A), dim=1), dim=1)[not_A])  # Explicit dim=1
-#     unmatchable_loss_B = -torch.mean(F.log_softmax(torch.cat((sigma_B, 1 - sigma_B), dim=1), dim=1)[not_B])  # Explicit dim=1
-
-#     loss = correspondence_loss_sum + unmatchable_loss_A + unmatchable_loss_B
-
-#     return loss
-    
-
+        loss.backward()  # Backward pass.
+        optimizer.step()  # Update model parameters.
         
+        total_loss += loss.item()
+
+    return total_loss / len(loader.dataset)       
 
 
 # Example usage:
+
+
+
+writer = SummaryWriter('logs/log6')
 
 data_path = './resize_photos'
    
 dataset = MyDataset(data_path)
 
 
+
+
+train_dataset=dataset
+train_loader = DataLoader(train_dataset, batch_size=10, shuffle=False)
+
 input_dim = 128  # Example input dimension// ?????
 output_dim = 128  # Example output dimension// ?????
 
 
 model = LightGlue(input_dim, output_dim)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-xA=torch.tensor(dataset[0]['des1'],dtype=torch.float32)
-xB=torch.tensor(dataset[0]['des2'],dtype=torch.float32)
-print(xA[0,0:5])
-sigma_A, sigma_B, P = model(xA, xB)
-loss = loss(P, dataset[0]['m'], dataset[0]['notA'], dataset[0]['notB'], sigma_A, sigma_B)
-print(loss)
+for epoch in range(1, 2):
+    loss = train(model, optimizer, train_loader)
+    print (loss)
+    writer.add_scalar('Training Loss', loss, epoch)
 
+writer.close()
 
-
-
+# xA=torch.tensor(dataset[0]['des1'],dtype=torch.float32)
+# xB=torch.tensor(dataset[0]['des2'],dtype=torch.float32)
+# print(xA[0,0:5])
+# sigma_A, sigma_B, P = model(xA, xB)
+# loss = loss(P, dataset[0]['m'], dataset[0]['notA'], dataset[0]['notB'], sigma_A, sigma_B)
+# print(loss)
 
 
 
@@ -136,40 +133,3 @@ print(loss)
 #         self.conv2 = GATConv(self.hid * self.in_head, out_channels, concat=False, heads=self.out_head, dropout=0.6)
         
 
-# def forward(self, data):
-#     iters = 1
-#     des1, des2 = data['des1'], data['des2']
-#     # des1 = F.normalize(des1, dim = 0)
-#     # des2 = F.normalize(des2, dim = 0)
-
-#     # inside_edge, cross_edge = self.get_edge_index(desc1, desc2)
-
-#     x = torch.Tensor(np.concatenate((des1, des2)))
-#     # for i in range(iters):
-#     #     print('x shape: ', x.shape)
-#     #     # print("x before conv1: ", x)
-#     #     x = self.conv1(x, inside_edge)
-#     #     # print("x after conv1: ", x)
-#     #     print('x shape: ', x.shape)
-#     #     x = F.elu(x)
-#     # x = self.conv2(x, cross_edge)
-
-#     xA = x[0:len(des1)]
-#     xB = x[len(des1):]
-
-#     S = torch.matmul(self.linear1(xA), self.linear1(xB).transpose(0, 1))
-
-#     sigma_A = torch.sigmoid(self.linear2(xA))
-#     sigma_B = torch.sigmoid(self.linear2(xB))
-
-#     softmax_SkA = F.softmax(S, dim=1)
-#     softmax_SkB = F.softmax(S, dim=0)
-
-#     P = torch.zeros_like(S)
-#     for i in range(S.size(0)):
-#         for j in range(S.size(1)):
-#             P[i, j] = sigma_A[i] * sigma_B[j] * softmax_SkB[i, :].sum() * softmax_SkA[:, j].sum()
-
-#     return P
-#     # p_match, match = sinkhorn_match2(des1, des2, self.DB_percentage)
-#     # return p_match, match
